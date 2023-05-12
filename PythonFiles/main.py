3aad9fdc352c586ca0362d6ac0b88394ee1c6ec8
froggorf <kjggr123@gmail.com>
2023-05-12 23:51:03 +0900

    openmap 파일 분리

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -1,30 +1,17 @@
 import tkinter as tk
+import openmap
 from cefpython3 import cefpython as cef
 
+
+
 root = tk.Tk()
-
-def message_loop_work():
-    cef.MessageLoopWork()
-    root.after(5,message_loop_work)
-
-def create():
-    window_info = cef.WindowInfo()
-    rect = [50,50,550,430]
-    window_info.SetAsChild(root.winfo_id(),rect)
-    cef.Initialize()
-    cef.CreateBrowserSync(window_info,url="https://weather.naver.com/map/02390118", window_title="test")
-    message_loop_work()
-
 def main():
 
-
-
     root.geometry('600x500')
-    b = tk.Button(root, text='ㅇㅇ', command=create)
+    b = tk.Button(root, text='ㅇㅇ', command= lambda r = root:openmap.create(r))
     b.pack()
     root.mainloop()
     cef.Shutdown()
-
 
 
 
---
+++ PythonFiles/openmap.py
@@ -0,0 +1,15 @@
+from cefpython3 import cefpython as cef
+
+
+def message_loop_work(root):
+    cef.MessageLoopWork()
+    #root.after(5,message_loop_work)
+    root.after(5, lambda r = root:message_loop_work(r))
+
+def create(root):
+    window_info = cef.WindowInfo()
+    rect = [50,50,550,430]
+    window_info.SetAsChild(root.winfo_id(),rect)
+    cef.Initialize()
+    cef.CreateBrowserSync(window_info,url="https://weather.naver.com/map/02390118", window_title="test")
+    message_loop_work(root)
