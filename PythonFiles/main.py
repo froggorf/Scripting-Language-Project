e4e8f1589a9156fcdd6683ccee07cd5a1e29889e
froggorf <kjggr123@gmail.com>
2023-05-13 14:31:43 +0900

    전면수정 (정상작동)
    탭 두개를 만들고 두번쨰 탭을 누르면 지도 생성, 다른 탭을 누르면 지도 사라지게 설정

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -1,81 +1,127 @@
 import tkinter as tk
 import tkinter.ttk
-
-import cefpython3.examples.tkinter_
-
-import openmap
 from cefpython3 import cefpython as cef
-from tkinter import messagebox
-
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 1000
 
-class MainGUI:
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.geometry(str(WINDOW_WIDTH) + 'x' + str(WINDOW_HEIGHT))
-        self.window.title("")
+def main():
+    root = tk.Tk()
+    app = MainFrame(root)
+    cef.Initialize()
+    app.mainloop()
+    cef.Shutdown()
 
-        self.mapView = None
+class MainFrame(tk.Frame):
+    def __init__(self, root):
+        self.browser_frame = None
+        self.show_browser_frame=False
+        self.navigation_bar = None
+        self.root = root
 
-        settings={}
-        cef.Initialize(settings = settings)
-        self.CreateInitUI()
+        # Root
+        root.geometry(str(WINDOW_WIDTH)+'x'+str(WINDOW_HEIGHT))
+        #root.geometry('900x600')
+        tk.Grid.rowconfigure(root, 0, weight=1)
+        tk.Grid.columnconfigure(root, 0, weight=1)
+
+        # MainFrame
+        tk.Frame.__init__(self, root)
+        self.master.title("기관지 지킴이")
+
+        #노트북 추가
+        self.notebook = tk.ttk.Notebook(root)
+        self.notebook.bind('<<NotebookTabChanged>>',self.my_notebook_msg)
+        self.notebook.grid(row = 0 , column = 0,sticky = tk.NW)
+
+        #탭1 추가
+        self.tab1_frame = tk.Frame(root)
+        self.note_tab1_image = tk.PhotoImage(file = 'Resource\\Note_Tab1.png')
+        self.notebook.add(self.tab1_frame,image=self.note_tab1_image)
+        tk.Label(self.tab1_frame,text="설정 지역 날씨").pack()
+
+        #탭2 추가
+        #self.tab2_frame=tk.Frame(self)
+        self.tab2_frame = tk.Frame(root)
+        self.note_tab2_image = tk.PhotoImage(file='Resource\\Note_Tab1.png')
+        self.notebook.add(self.tab2_frame, image=self.note_tab2_image)
+        #tk.Label(self.tab2_frame, text="지도").pack()
 
 
-        self.window.mainloop()
-        cef.Shutdown()
-    def CreateInitUI(self):
-        self.notebook = tk.ttk.Notebook(self.window)
-        self.notebook.bind('<<NotebookTabChanged>>',self.my_msg)
-        self.notebook.pack(padx=10,pady=10)
+        # BrowserFrame
+        #self.browser_frame = None
+        self.browser_frame = BrowserFrame(self)
+        #self.browser_frame.grid(row=0, column=0,
+        #                        sticky=(tk.N + tk.S + tk.E + tk.W))
+        #tk.Grid.rowconfigure(self, 0, weight=1)
+        #tk.Grid.columnconfigure(self, 0, weight=1)
 
-        #1번탭 관련
-        self.tab1_frame = tk.Frame(self.window)
-        self.note_tab1_image = tk.PhotoImage(file=  'Resource\\Note_Tab1.png')
+        # Pack MainFrame
+        #self.pack(fill=tk.BOTH, expand=tk.YES)
 
-        self.notebook.add(self.tab1_frame, image=self.note_tab1_image)
-        tk.Label(self.tab1_frame, text="설정 지역 날씨").pack()
 
-        self.tab2_frame = tk.Frame(self.window)
-        self.note_tab2_image = tk.PhotoImage(file = 'Resource\\Note_Tab1.png')
-        self.notebook.add(self.tab2_frame,image=self.note_tab2_image)
-        self.tab2_frame.pack_forget()
+
+
+    def my_notebook_msg(self,_):
+        if(self.show_browser_frame==True):
+            self.show_browser_frame=False
+            self.browser_frame.grid_remove()
+            self.grid_remove()
+
+
+        select_notetab_index =self.notebook.index("current")
+
+
+        if(select_notetab_index==0):
+            #0번탭에 해당하는 함수를 진행
+            pass
+        elif(select_notetab_index==1):
+            #1번탭에 해당하는 함수를 진행
+            self.browser_frame.grid(row=0, column=0,
+                                sticky=(tk.N + tk.S + tk.E + tk.W))
+
+            tk.Grid.rowconfigure(self, 0, weight=1)
+            tk.Grid.columnconfigure(self, 0, weight=1)
+            self.grid(row=3,column=0,ipadx = 500,ipady=300,sticky=tk.NW)
+
+            tk.Grid.rowconfigure(self, 0, weight=1)
+            tk.Grid.columnconfigure(self, 0, weight=1)
+            self.show_browser_frame = True
+
+
+
+
+class BrowserFrame(tk.Frame):
+    def __init__(self, mainframe):
+        self.browser = None
+        tk.Frame.__init__(self, mainframe)
+        self.mainframe = mainframe
+        self.bind("<Configure>", self.on_configure)
+
+    def embed_browser(self):
+        window_info = cef.WindowInfo()
+        rect = [50,50, self.winfo_width()-50, self.winfo_height()-50]
+        print(rect)
+        window_info.SetAsChild(self.get_window_handle(), rect)
+        self.browser = cef.CreateBrowserSync(window_info,
+                                             url="https://weather.naver.com/map/02390118")
+        self.message_loop_work()
+
+    def get_window_handle(self):
+        return self.winfo_id()
 
     def message_loop_work(self):
         cef.MessageLoopWork()
-        # root.after(5,message_loop_work)
-        self.window.after(10, self.message_loop_work)
+        self.after(10, self.message_loop_work)
 
-    def createMap(self):
-        self.window_info = cef.WindowInfo()
-        self.rect = [100,500,WINDOW_WIDTH-50,WINDOW_HEIGHT-100]
-        self.window_info.SetAsChild(self.window.winfo_id(), self.rect)
-
-        self.mapView = cef.CreateBrowserSync(self.window_info, url="https://weather.naver.com/map/02390118", window_title="test")
-        self.message_loop_work()
-
-    def my_msg(self,event):
-        if(self.mapView):
-            print('엥1')
-            cef.QuitMessageLoop()
-            #cef.Initialize()
-
-            #self.mapView = None
-            print('엥')
-
-        index = self.notebook.index("current")
-        if index == 1:
-            self.createMap()
-        print(index)
+    def on_configure(self, _):
+        if not self.browser:
+            self.embed_browser()
 
 
 
 if __name__ == '__main__':
-    MainGUI()
-
-
+    main()
 
 
 #지도 넣는 버튼
