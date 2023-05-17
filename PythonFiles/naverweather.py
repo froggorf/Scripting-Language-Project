de9288a8b29bfe736334bdd87280ad6a9591f1ba
froggorf <kjggr123@gmail.com>
2023-05-18 02:00:26 +0900

    시간당 날씨 정보 받아오기 추가

--- PythonFiles/naverweather.py
+++ PythonFiles/naverweather.py
@@ -18,8 +18,30 @@ def GetWeatherInformation():
     #날씨상태
     weather_state = soup.find("span",{"class":"weather before_slash"}).text
     #print(weather_state)
+    #<div class="forecast_wrap _selectable_tab">
+    temperature_per_hour = GetTemperaturePer2Hour(soup.find("div",{"class":"forecast_wrap _selectable_tab"}))
+    return location,temperature,weather_state, temperature_per_hour
 
-    return location,temperature,weather_state
+def GetTemperaturePer2Hour(soup):
+    time = soup.findAll("dt",{"class":"time"})
+    weather_state = soup.findAll("span",{"class":"blind"})
+    degree = soup.findAll("span", {"class": "num"})
+    all_time = list()
+    all_weather_state = list()
+    all_degree = list()
+    for i in range(48):
+        all_time.append(time[i].text)
+        all_weather_state.append(weather_state[i].text)
+        all_degree.append(int(degree[i].text[:-1]))
+
+
+
+
+    return all_time,all_weather_state,all_degree
+
+
+
+
 
 
 
