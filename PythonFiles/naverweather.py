9939f66197fa033ac0e920ba89d3b485ce4d793d
froggorf <kjggr123@gmail.com>
2023-05-18 22:01:29 +0900

    주간 날씨 정보 받아오기 구현 완료

--- PythonFiles/naverweather.py
+++ PythonFiles/naverweather.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-
+mainsoup = None
 
 def GetWeatherInformation():
     html = requests.get('https://search.naver.com/search.naver?query=날씨')
     soup = bs(html.text, 'html.parser')
 
+    global mainsoup
+    mainsoup=soup
     #위치
     location = soup.find("h2",{"class":"blind"}).text
     #print(location)
@@ -20,6 +22,9 @@ def GetWeatherInformation():
     #print(weather_state)
     #<div class="forecast_wrap _selectable_tab">
     temperature_per_hour = GetTemperaturePer2Hour(soup.find("div",{"class":"forecast_wrap _selectable_tab"}))
+
+    #GetWeekWeatherForecast(soup)
+
     return location,temperature,weather_state, temperature_per_hour
 
 def GetTemperaturePer2Hour(soup):
@@ -33,12 +38,47 @@ def GetTemperaturePer2Hour(soup):
         all_time.append(time[i].text)
         all_weather_state.append(weather_state[i].text)
         all_degree.append(int(degree[i].text[:-1]))
-
-
-
-
     return all_time,all_weather_state,all_degree
 
+def GetWeekWeatherForecast():
+    week_weathers_from_naver = mainsoup.findAll("span", {"class":"weather_inner"})
+    weather_state_am = list()
+    weather_state_pm = list()
+    rain_fall_am = list()
+    rain_fall_pm = list()
+    for index, data in enumerate(week_weathers_from_naver):
+        ws = data.find("span",{"class":"blind"}).text
+        if index % 2 == 0 :
+            weather_state_am.append(ws)
+            rain_fall_am.append(data.find("span", {"class": "rainfall"}).text)
+        else:
+            weather_state_pm.append(ws)
+            rain_fall_pm.append(data.find("span", {"class": "rainfall"}).text)
+
+
+
+
+    week_data =mainsoup.findAll("div",{"class":"cell_date"})
+    all_week = list()
+    for index,data in enumerate(week_data):
+        all_week.append((data.find("strong",{"class":"day"}).text, data.find("span",{"class":"date"}).text[:-1]))
+    #print(all_week)
+    #print(weather_state_am)
+    #print(weather_state_pm)
+    #print(rain_fall_am)
+    #print(rain_fall_pm)
+
+    max_temp_data = mainsoup.findAll("span",{"class":"highest"})
+    min_temp_data = mainsoup.findAll("span", {"class": "lowest"})
+
+    max_temp = list()
+    min_temp = list()
+    for data in max_temp_data:
+        max_temp.append(data.text[4:])
+    for data in min_temp_data:
+        min_temp.append(data.text[4:])
+
+    return all_week, weather_state_am, weather_state_pm, rain_fall_am, rain_fall_pm, max_temp, min_temp
 
 
 
@@ -49,3 +89,4 @@ def GetTemperaturePer2Hour(soup):
 
 if __name__ == '__main__':
     GetWeatherInformation()
+    GetWeekWeatherForecast()
