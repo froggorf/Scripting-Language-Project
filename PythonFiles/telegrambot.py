3be6b3afe9b3a3940dd1c8af21f8f67edef1289b
Ko-jung <sm000915@naver.com>
2023-05-25 00:41:41 +0900

    TelegramBot class staticmathod로 변경, 옵션에 따라 출력물 바뀌게 설정

--- PythonFiles/telegrambot.py
+++ PythonFiles/telegrambot.py
@@ -1,23 +1,35 @@
 from telegram import Bot
 import requests
 from datetime import datetime
+import option
+
 
 class TelegramBot:
-    def __init__(self):
+    __TOKEN = "6111372734:AAH-AdTXdJDTX7UDfRqdCFnVck1ewGi4-AM"
+    __chat_id = "-1001944555399"
+    @staticmethod
+    def sendParticulateMessage(location, optionDict):
+        str = "/"
+        for k, v in optionDict.items():
+            if v:
+                str = str + k + "/"
+
+        sendText = datetime.today().strftime("%p %I:00 - ") + location + str
+
+        data = {"chat_id": TelegramBot.__chat_id, "text": sendText}
+        url = f"https://api.telegram.org/bot{TelegramBot.__TOKEN}/sendMessage?"
+        res = requests.post(url, json=data)
+        # print(res.json())
+
+    @staticmethod
+    def makeParticulateToStr():
+
         pass
 
-    def sendMessage(self):
-        __TOKEN = "6111372734:AAH-AdTXdJDTX7UDfRqdCFnVck1ewGi4-AM"
-        __chat_id = "-1001944555399"
-
-
-        sendText = datetime.today().strftime("%p %I:00 - ")
-
-        data = {"chat_id": __chat_id, "text": sendText}
-        url = f"https://api.telegram.org/bot{__TOKEN}/sendMessage?"
-        res = requests.post(url, json=data)
-        print(res.json())
 
 if __name__ == '__main__':
-    t = TelegramBot()
-    t.sendMessage()
+    # t = TelegramBot()
+    o = option.Option()
+    o.beGreat = True
+    o.beDry = True
+    TelegramBot().sendParticulateMessage('정왕동', o.__dict__)
