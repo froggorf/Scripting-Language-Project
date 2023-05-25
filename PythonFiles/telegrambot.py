520356b2c7bf535484bacd60ab0166c3244a7a4e
Ko-jung <sm000915@naver.com>
2023-05-25 11:08:38 +0900

    텔레그램 봇 설정에 따른 메세지 구현 중

--- PythonFiles/telegrambot.py
+++ PythonFiles/telegrambot.py
@@ -3,16 +3,17 @@ import requests
 from datetime import datetime
 import option
 
-
+options = option.Option()
 class TelegramBot:
     __TOKEN = "6111372734:AAH-AdTXdJDTX7UDfRqdCFnVck1ewGi4-AM"
     __chat_id = "-1001944555399"
+    optionDict = options.__dict__
     @staticmethod
     def sendParticulateMessage(location, optionDict):
         str = "/"
         for k, v in optionDict.items():
             if v:
-                str = str + k + "/"
+                str = str + textDict[k] + "/"
 
         sendText = datetime.today().strftime("%p %I:00 - ") + location + str
 
@@ -26,10 +27,27 @@ class TelegramBot:
 
         pass
 
+textDict = {
+        "beRainy"    : list(),
+        "beSunny"    : list(),
+        "beCloudy"   : list(),
+        "beClear"    : list(),
+        "beDry"      : list(),
+        "beHumid"    : list(), # 습함
+        "beFoggy"    : list(),
+        "beLighting" : list(),
+        "beWindy"    : list(),
+
+        "beGreat"    : "좋음",
+        "beNormal"   : "보통",
+        "beBad"      : "나쁨",
+        "beTooBad"   : "매우나쁨",
+}
 
 if __name__ == '__main__':
     # t = TelegramBot()
+
     o = option.Option()
     o.beGreat = True
-    o.beDry = True
+    o.beNormal = True
     TelegramBot().sendParticulateMessage('정왕동', o.__dict__)
